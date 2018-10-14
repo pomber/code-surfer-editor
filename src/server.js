10ce99df1328261761569a1441278603ca731e16
pomber <pombopombopombo@gmail.com>
2018-10-14 17:11:57 -0300

    Change oembed response

--- src/server.js
+++ src/server.js
@@ -9,20 +9,22 @@ import printHtml from "./page-template";
 const assets = require(process.env.RAZZLE_ASSETS_MANIFEST);
 
 const getOembed = (req, res) => {
-  res.status(200).set("Content-Type", "application/json").send(`
-    {
-      "version": "1.0",
-      "type": "rich",
-      "provider_name": "Code Surfer",
-      "provider_url": "https://code-surfer.now.sh/",
-      "title": "Code Surfer",
-      "author_name": "",
-      "author_url": "",
-      "width": 480,
-      "height": 270,
-      "html": "<iframe src=\"https://code-surfer.now.sh/\" height=\"270.0\" width=\"480\" frameborder=\"0\" scrolling=\"no\"></iframe>"
-    }    
-  `);
+  res
+    .status(200)
+    .set("Content-Type", "application/json")
+    .send({
+      version: "1.0",
+      type: "rich",
+      provider_name: "Code Surfer",
+      provider_url: "https://code-surfer.now.sh/",
+      title: "Code Surfer",
+      author_name: "",
+      author_url: "",
+      width: 480,
+      height: 270,
+      html:
+        '<iframe src="https://code-surfer.now.sh/" height="270.0" width="480" frameborder="0" scrolling="no"></iframe>'
+    });
 };
 const getCodeSurfer = (req, res) => {
   const markup = inline(renderToStaticMarkup(<App />));
