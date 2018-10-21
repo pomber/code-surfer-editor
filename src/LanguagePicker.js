fb21602ea38a5b7d552fd5bcd4fadc462ddf6515
pomber <pombopombopombo@gmail.com>
2018-10-21 12:35:33 -0300

    Faster LanguagePicker

--- src/LanguagePicker.js
+++ src/LanguagePicker.js
@@ -5,13 +5,15 @@ const languages = Object.keys(Prism.languages)
   .filter(key => typeof Prism.languages[key] !== "function")
   .sort();
 
+const languageElements = languages.map(language => (
+  <option value={language} key={language}>
+    {language}
+  </option>
+));
+
 const LanguagePicker = ({ value, onChange }) => (
   <select value={value} onChange={e => onChange(e.target.value)}>
-    {languages.map(language => (
-      <option value={language} key={language}>
-        {language}
-      </option>
-    ))}
+    {languageElements}
   </select>
 );
 
