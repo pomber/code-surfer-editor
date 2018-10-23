6b7b32936397cb6faf3fda25ea4f5bec343790d0
pomber <pombopombopombo@gmail.com>
2018-10-23 17:27:59 -0300

    Refactor html

--- src/editor.server.js
+++ src/editor.server.js
@@ -3,6 +3,7 @@ import { renderToString } from "react-dom/server";
 import inline from "glamor/inline";
 import { readStateFromPath } from "./utils/state-parser";
 import Editor from "./components/Editor";
+import * as html from "./html-parts";
 
 const assets = require(process.env.RAZZLE_ASSETS_MANIFEST);
 
@@ -14,56 +15,19 @@ export function getEditor(request, response) {
   const markup = inline(renderToString(<Editor initialState={state} />));
   const protocolAndHost = request.protocol + "://" + request.get("host");
   const fullUrl = protocolAndHost + request.originalUrl;
-  //TODO move hardcoded styles to glamor or something
-  response.status(200).send(
-    `<!doctype html>
-<html lang="">
 
-<head>
-  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-  <meta charset="utf-8" />
-  <title>Code Surfer Editor</title>
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link 
-    href="${protocolAndHost}/oembed?url=${encodeURIComponent(fullUrl)}" 
-    rel="alternate"
-    type="application/json+oembed" 
-    title="Code Surfer oEmbed" />
-  <style>
-    body {
-      margin: 0;
-      padding: 0;
-      font-family: sans-serif;
-    }
-    html,
-    body,
-    #root {
-      height: 100%;
-      padding: 0;
-      margin: 0;
-    }
-    
-    .react-codemirror2,
-    .CodeMirror {
-      height: 100% !important;
-    }
-  </style>
-  ${
-    assets.editor.css
-      ? `<link rel="stylesheet" href="${assets.editor.css}">`
-      : ""
-  }
-  ${
-    process.env.NODE_ENV === "production"
-      ? `<script src="${assets.editor.js}" defer></script>`
-      : `<script src="${assets.editor.js}" defer crossorigin></script>`
-  }
-</head>
+  const content = `
+  <!doctype html>
+  <html lang="">
+  <head>
+    ${html.headContent()}
+    ${html.oembedLink(protocolAndHost, fullUrl)}
+    ${html.style()}
+    ${html.script(assets.editor.js)}
+  </head>
+  ${html.body(markup)}
+  </html>
+  `.trim();
 
-<body>
-  <div id="root">${markup}</div>
-</body>
-
-</html>`
-  );
+  response.status(200).send(content);
 }
--- src/frame.server.js
+++ src/frame.server.js
@@ -3,6 +3,7 @@ import { renderToString, renderToStaticMarkup } from "react-dom/server";
 import inline from "glamor/inline";
 import { readStateFromPath } from "./utils/state-parser";
 import CodeSurferContainer from "./components/CodeSurferContainer";
+import * as html from "./html-parts";
 
 const assets = require(process.env.RAZZLE_ASSETS_MANIFEST);
 
@@ -29,43 +30,19 @@ export function getFrame(request, response) {
   );
   const protocolAndHost = request.protocol + "://" + request.get("host");
   const fullUrl = protocolAndHost + request.originalUrl;
-  response.status(200).send(`<!doctype html>
+
+  const content = `
+  <!doctype html>
   <html lang="">
-  
   <head>
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta charset="utf-8" />
-    <title>Code Surfer Editor</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link 
-      href="${protocolAndHost}/oembed?url=${encodeURIComponent(fullUrl)}" 
-      rel="alternate"
-      type="application/json+oembed" 
-      title="Code Surfer oEmbed" />
-    <style>
-      body {
-        margin: 0;
-        padding: 0;
-        font-family: sans-serif;
-      }
-      html,
-      body,
-      #root {
-        height: 100%;
-        padding: 0;
-        margin: 0;
-      }
-    </style>
-    ${
-      process.env.NODE_ENV === "production"
-        ? `<script src="${assets.frame.js}" defer></script>`
-        : `<script src="${assets.frame.js}" defer crossorigin></script>`
-    }
+    ${html.headContent()}
+    ${html.oembedLink(protocolAndHost, fullUrl)}
+    ${html.style()}
+    ${html.script(assets.frame.js)}
   </head>
-  
-  <body>
-    <div id="root">${markup}</div>
-  </body>
-  
-  </html>`);
+  ${html.body(markup)}
+  </html>
+  `.trim();
+
+  response.status(200).send(content);
 }
